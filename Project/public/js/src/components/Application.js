3ab0c768b7954b6f94bbb4bbdd79e2ff4f6afade
ily213 <84005299+ily213@users.noreply.github.com>
2021-06-02 14:21:30 +0400

    1

---
+++ Project/public/js/src/components/Application.js
@@ -0,0 +1,24 @@
+
+export class Application {
+    constructor() {
+        this.view
+        this.employee
+        this.tasks
+        this.projects
+        this.info
+        this.toolbar
+        this.mainWindow
+    }
+
+    init() {
+
+    }
+
+    attachEvents() {
+
+    }
+
+    config() {
+        return
+    }
+}
